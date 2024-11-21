f9fddffef930ee64b0ca5088ff69628215795ab1
Matteo Risso <144807707+RissoMatteo@users.noreply.github.com>
2024-11-21 22:30:46 +0100

    Add files via upload

---
+++ Html/ripassso/interrogazione1.js
@@ -0,0 +1,22 @@
+[
+    {
+        "nome": "Mario",
+        "cognome": "Rossi",
+        "telefono": "333 1234567",
+        "lingue": ["GB", "IT", "FR"]
+    },
+
+    {
+        "nome":"Roberta",
+        "cognome":"Molinari",
+        "telefono":"0171 343434",
+        "lingue": ["IT", "FR"]
+    },
+
+    {
+        "nome":"Karol",
+        "cognome":"Bruno",
+        "telefono":"333 4436998",
+        "lingue": ["GB", "IT"]
+    }
+]
